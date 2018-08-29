a1b633cae3e763c6f577a8d9d3685aeab7251626
AlexH <estudiantepb08@gmail.com>
2018-08-28 20:31:26 -0500

    Jquery de la calculadora

---
+++ js/jqueryOperaciones.js
@@ -0,0 +1,31 @@
+
+function fnejecutar(operacion){
+    var resultado;
+
+switch(operacion){
+    case "sumar":
+    function fsuma(numero1, numero2){       
+        resultSuma = parseInt(numero1) + parseInt(numero2);
+        return resultado;
+    }break;
+
+    case "restar":
+    function frestar(numero1, numero2){        
+        resultResta = parseInt(numero1) - parseInt(numero2);
+        return resultado
+    }break;
+
+    case "multiplicar":
+    function fmultiplicar(numero1, numero2){       
+        resultado = parseInt(numero1) * parseInt(numero2);
+        return resultado
+    }break;
+
+    case "dividir":
+    function fdividir(numero1, numero2){       
+        resultado = parseInt(numero1) / parseInt(numero2);
+        return resultado
+    }break;
+    
+}
+}
